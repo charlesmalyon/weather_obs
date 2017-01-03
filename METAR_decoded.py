b6816d58db7b2ccef7f74aa4776354b8e91e5558
Edd Williams <edd.williams@me.com>
2017-01-03 23:51:00 +0000

    Update METAR_decoded.py
    adding some notes about changes to code.

--- METAR_decoded.py
+++ METAR_decoded.py
@@ -5,6 +5,11 @@ import numpy as np
 import string
 import subprocess
 
+#should put all of this in a function that can be re-run overtime. 
+
+#making a list(array) for historical data. not yet used. 
+history = [] 
+
 #manual input of ICAO index
 index = raw_input("Please enter your input")
 
@@ -20,6 +25,10 @@ print(temp_index)
 #print out temp
 temp = report[temp_index+4:temp_index+6]
 print(temp)
+
+# Probably not the right suytax for adding to an array 
+temp.push()  
+
 #FTP of METAR reports
 ftp = 'http://tgftp.nws.noaa.gov/data/observations/metar/stations/'
 
