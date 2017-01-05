7fe3b8362508e817bbc46ea5eee5ccfb35256d95
Edd Williams <edd.williams@me.com>
2017-01-05 17:26:39 +0000

    Add files via upload

--- METAR_decoded.py
+++ METAR_decoded.py
@@ -1,9 +1,84 @@
 from metar import Metar
 from METAR_download import getCode
 
-code = getCode()
+import plotlyLogin
 
-obs = Metar.Metar(code)
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-print("temperature: %s" % obs.temp.string("C"))
+login = plotlyLogin.username
+api = plotlyLogin.api
 
+py.sign_in(login, api)
+
+# You can put in the brakets how far back you want to go (in hours)
+
+codes = getCode(10)
+
+# print(codes)
+
+obs = []
+
+# Fills the array 'obs' with METAR objects
+for i in range(0, len(codes)):
+	obs.append(Metar.Metar(codes[i]))
+
+# From this you can call any object in the array and ask for its METAR properties
+# Print is just an example of this
+
+# print(obs[4].temp)
+
+# Arrays to hold all of this specific data.
+tempTimes = []  # This is a really awkward datetime.datetime object
+tempWindSpeeds = []  # This doesn't help because its still in METAR format
+tempTemps = []  # Also in an annoying METAR format
+
+for i in range(0, len(codes)):
+	tempTimes.append(obs[i].time)
+	tempWindSpeeds.append(obs[i].wind_speed)
+	tempTemps.append(obs[i].temp)
+
+dates = []  #Bit of an afterthought but easier to see when it was in output
+
+times = []
+temps = []
+winds = []
+
+
+# If you want the raw text with "C" and "knots" use the 'temp' arrays before I have adjusted them.
+
+# This was a right ball ache
+# I had to convert back and forth in order to concatonate it right
+# This only deals with the 'times' array
+for i in range(0, len(codes)):
+	times.append(int((str(tempTimes[i].hour)) + str(tempTimes[i].minute)))
+	dates.append(int((str(tempTimes[i].month) + str(tempTimes[i].day))))
+
+# The 'temps' needed to be converted to a string in order to put them into
+# an array. God knows why. There must be an easier way to do that.
+for i in range(0, len(codes)):
+	temps.append(str(tempTemps[i]))
+	temps[i] = temps[i][0:-2]  #Removes the 'C' at the end
+
+# Same again for 'windspeed'. This could bite me later.
+for i in range(0, len(codes)):
+	winds.append(str(tempWindSpeeds[i]))
+	winds[i] = winds[i][0:-6]
+
+trace0 = go.Scatter(
+	x = times,
+	y = temps,
+	mode = 'lines',
+	name = 'Temperatures (In C)'
+)
+
+trace1 = go.Scatter(
+	x = times,
+	y = winds,
+	mode = 'lines',
+	name = 'Wind Speed (In Knots)'
+)
+
+data = [trace0, trace1]
+
+py.plot(data, filename = 'test')
--- METAR_download.py
+++ METAR_download.py
@@ -1,16 +1,20 @@
 import urllib2
 import datetime as datetime
 
-def getCode():
+def getCode(shift):
+
+	# This will be how far back the data goes (In Hours)
+	timeShift = shift
+
 	# index = raw_input("Please type in your index")
 
 	# setting up the time variables
 	now = datetime.datetime.now()
 	year, month, day, hour, minute = now.year, now.month, now.day, now.hour, now.minute
-	starthour = hour -1
+	starthour = (hour - timeShift)
 
 	# setting up the url
-	url = 'http://www.ogimet.com/display_metars2.php?lang=en&lugar=EGSS+03683&tipo=SA&ord=REV&nil=SI&fmt=txt&ano=%s&mes=%s&day=%s&hora=%s&anof=%s&mesf=%s&dayf=%s&horaf=%s&minf=%s&send=send' % (year,month,day,starthour,year,month,day,hour,minute)
+	url = 'http://www.ogimet.com/display_metars2.php?lang=en&lugar=EGSS+03683&tipo=SA&ord=REV&nil=SI&fmt=txt&ano=%s&mes=%s&day=%s&hora=%s&anof=%s&mesf=%s&dayf=%s&horaf=%s&minf=%s&send=send' % (year, month, day, starthour, year, month, day, hour, minute)
 
 	# taking data from url
 	file = []
@@ -23,14 +27,41 @@ def getCode():
 	# Find the indicies whith the use of searchMetar in.
 	indicies = [i for i, s in enumerate(file) if searchMetar in s]
 
+	# print(indicies)
+
+	dataArray = []
+
+
+	# Fill an array with the data from the indicies we want.
+	for i in indicies:
+		dataArray.append(file[i])
+
+	arrayEnd = int(len(dataArray)-2)
+
+	del dataArray[0]
+
+	del dataArray[arrayEnd]
+
+	metarArray = dataArray
+
+	for i in range(0,len(metarArray)):
+		tempLen = len(metarArray[i])
+		metarArray[i] = metarArray[i][13:tempLen-2]
+		# print(metarArray[i])
+
+	# print(metarArray)
+	return metarArray
+
 	# The Second in the list(array) will have the one we want.
-	fileTemp = file[indicies[1]]
+	# fileTemp = file[indicies[1]]
 
 	# Narrow the string down to the right length to input into the other python script
-	metarString = fileTemp[13:(len(fileTemp)-2)]
+	# metarString = fileTemp[13:(len(fileTemp)-2)]
 
 	# Outputs the string we need for the other function.
-	return metarString
+	# return metarString
+
+# getCode(10)
 
 
 
